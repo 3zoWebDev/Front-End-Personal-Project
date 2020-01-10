6cdb1c5fa993b9f6ba3fe3b17960b4761e68cf93
3zoDev <3zoalsubhi@gmail.com>
2020-01-10 19:28:51 +0300

    Finshed digital clock project

--- Digital-Clock/index.js
+++ Digital-Clock/index.js
@@ -1,1 +1,11 @@
-alert("Connected");
+function clock(){
+let date = new Date();
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second= document.querySelector(".second");
+hour.innerText = date.getHours();
+minute.innerText = date.getMinutes();
+second.innerText = date.getSeconds();
+}
+setInterval(clock,100);
+//console.log(date.getHours());
